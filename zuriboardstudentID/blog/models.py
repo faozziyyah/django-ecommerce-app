c4f4f9b333f4ef5b8021e8649734503702036a81
Your name <omowunmidaud1@gmail.com>
2022-06-24 23:02:11 +0100

    updated

--- zuriboardstudentID/blog/models.py
+++ zuriboardstudentID/blog/models.py
@@ -1,13 +1,39 @@
+from django.contrib.auth.models import get_user_model
 from django.db import models
-from django.contrib.auth.models import User
+from django.utils import timezone
+from django.template.defaultfilters import slugify
+from django.urls import reverse
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length= 200)
-    text = models.TextField()
-    author = models.ForeignKey(User, related_name="author", on_delete=models.CASCADE)
-    created_date = models.DateTimeField(auto_now_add=True)
-    published_date = models.DateTimeField(auto_now_add=True)
+
+    STATUS_CHOICES = (
+        ("draft", "draft"),
+        ("published", "published")
+    )
+
+#DB Fields
+    title = models.CharField(max_length= 250)
+    slug = models.SlugField(max_length=300, unique=True, editable=False)
+    author = models.ForeignKey(
+        get_user_model(), blank=True, null=True, related_name="blog_posts", on_delete=models.CASCADE
+    )
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    publish = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
+
+    class Meta:
+        ordering = ("-publish",)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+        pass
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", kwargs={'slug': self.slug})
